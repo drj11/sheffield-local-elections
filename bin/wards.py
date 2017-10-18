7c700c8e358e9ead68666d10e260326c17c4db2f
David Jones <drj@pobox.com>
2017-10-18 22:22:12 +0100

    Flatten CSV somewhat

--- bin/wards.py
+++ bin/wards.py
@@ -1,27 +1,37 @@
 #!/usr/bin/env python3
 
+import csv
 import re
 import sys
 
 def as_wards(inp):
-    import csv
-
     rows = csv.reader(inp)
+    ward = None
     for row in rows:
+        if row[0].startswith('Elected'):
+            yield [ward, "elected"] + row
+            continue
+        if row[0].startswith('Electorate'):
+            yield [ward, "turnout"] + row
+            continue
         if re.search(r'\d', row[0]):
-            yield [0] + row
+            yield [ward, "candidate"] + candidate(row)
             continue
-        if re.match(r'Elected:|Electorate', row[0]):
-            yield [0] + row
-            continue
-        yield [1] + row
+        ward = row[0]
 
 def wards(inp):
+    out = csv.writer(sys.stdout)
     for row in as_wards(inp):
-        if row[0]:
-            print("\nWARD: {}\n".format(row[1]))
-        else:
-            print(row[1])
+        out.writerow(row)
+
+def candidate(row):
+    """Convert (1-element) list into more structured candidate
+    list."""
+
+    m = re.match(r'(.*),\s*([^,]*):\s*(.*)', row[0])
+    cells = list(m.groups())
+    cells[-1] = cells[-1].replace(',', '')
+    return cells
     
 
 def main():
